c95f8d765431e7d0740ac62db5a57c6afa4f2d92
Bazz17 <106252065+Bazz17@users.noreply.github.com>
2024-12-18 15:36:55 +0100

    Delete eco directory

--- eco/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-from django.contrib.auth.models import User
-
-
-model_list = [Habit, UserHabit, ActivityLog, Goal, Badge, UserBadge, Tip]
-admin.site.register(model_list)
-
